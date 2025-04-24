516103214f89922042c4e58885a0e3a7615aafe7
yaralahruthik <yaralahruthikreddy@gmail.com>
2025-04-24 14:36:36 +0530

    learn setup

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,20 @@
+import { buttonVariants } from 'fumadocs-ui/components/ui/button';
 import Link from 'next/link';
 
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">
-        You can open{' '}
-        <Link
-          href="/docs"
-          className="text-fd-foreground font-semibold underline"
-        >
-          /docs
-        </Link>{' '}
-        and see the documentation.
-      </p>
+      <h1 className="mb-4 text-2xl font-bold">
+        Learn Core Frontend and Product Skills For Real World
+      </h1>
+      <h2 className="mb-4 text-lg">
+        Work on Practical Stuff And Get Hired or Build Your Own Product
+      </h2>
+      <div>
+        <Link className={buttonVariants({ color: 'primary' })} href="/learn">
+          Learn Now
+        </Link>
+      </div>
     </main>
   );
 }
