b472188034f07c40e924a2745fa910907681ef54
yaralahruthik <yaralahruthikreddy@gmail.com>
2025-05-07 23:45:01 +0530

    head update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Banner } from 'fumadocs-ui/components/banner';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 import type { ReactNode } from 'react';
 import './global.css';
 
@@ -11,11 +12,11 @@ const inter = Inter({
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
-      <script
+      <Script
         defer
         data-domain="frontendhire.com"
         src="https://plausible.io/js/script.js"
-      ></script>
+      ></Script>
       <body className="flex min-h-screen flex-col">
         <Banner variant="rainbow" id="launch">
           We revamped our site to better serve our users!
