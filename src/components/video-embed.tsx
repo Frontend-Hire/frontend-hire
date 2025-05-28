29a5821525d25739f8580bdd7274bce326bd79f1
yaralahruthik <yaralahruthikreddy@gmail.com>
2025-05-28 10:12:01 +0530

    video embed refactoring

--- src/components/video-embed.tsx
+++ src/components/video-embed.tsx
@@ -1,15 +1,17 @@
 export default function VideoEmbed({
   id,
   title,
+  startAt,
 }: {
   id: string;
   title: string;
+  startAt?: number;
 }) {
   return (
     <iframe
       width="100%"
       className="aspect-video"
-      src={`https://www.youtube.com/embed/${id}`}
+      src={`https://www.youtube.com/embed/${id}?start=${startAt || 0}`}
       title={title}
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       allowFullScreen
