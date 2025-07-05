57f5a9f482d1e7abdc8d58473f2d2f77a48057d9
yaralahruthik <yaralahruthikreddy@gmail.com>
2025-07-05 21:17:35 +0530

    og

--- src/app/learn/[[...slug]]/page.tsx
+++ src/app/learn/[[...slug]]/page.tsx
@@ -66,15 +66,25 @@ export async function generateStaticParams() {
   return source.generateParams();
 }
 
-export async function generateMetadata(props: {
+export async function generateMetadata({
+  params,
+}: {
   params: Promise<{ slug?: string[] }>;
 }) {
-  const params = await props.params;
-  const page = source.getPage(params.slug);
+  const { slug = [] } = await params;
+  const page = source.getPage(slug);
   if (!page) notFound();
 
+  const image = ['/docs-og', ...slug, 'image.png'].join('/');
   return {
     title: page.data.title,
     description: page.data.description,
+    openGraph: {
+      images: image,
+    },
+    twitter: {
+      card: 'summary_large_image',
+      images: image,
+    },
   };
 }
